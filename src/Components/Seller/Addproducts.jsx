1d466ada1247472c10edbc3316a0d28a9ef476d1
yash kumar mishra <mishrayash3778@gmail.com>
2024-02-27 20:33:48 +0530

    j

--- src/Components/Seller/Addproducts.jsx
+++ src/Components/Seller/Addproducts.jsx
@@ -126,16 +126,14 @@ function Edit() {
         </div>
 
         <div className="relative z-0 w-full mb-6 group">
-          <input
-            type="text"
-            id="sex"
-            name="sex"
-            value={category}
-            onChange={(e) => setcategory(e.target.value)}
-            required // Mark the field as required
-            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-            placeholder=" "
-          />
+          	<select id="Com" className="text-base text-gray-800 outline-none border-2 px-4 py-2 rounded-lg" onChange={(e)=>{
+            setcategory(e.target.value)
+          }}>
+            <option value="electronics">electronics</option>
+            <option value="jwellery">jewelery</option>
+            <option value="men">men's clothing</option>
+            <option value="women">women's clothing</option>
+          </select>
           <label
             for="sex"
             className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
